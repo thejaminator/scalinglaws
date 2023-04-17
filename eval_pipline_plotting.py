b7512dad266699c95fb40eeeda6ac34b4c1f44e1
James Chua <james@leadiq.com>
2023-04-17 19:24:11 +0800

    save plotting

--- eval_pipline_plotting.py
+++ eval_pipline_plotting.py
@@ -7,9 +7,12 @@ from eval_pipeline.main import load_data, run_model, load_df
 from eval_pipeline.plot_loss import plot_loss, plot_classification_loss
 from tqdm import tqdm
 import plotly.express as px
+import plotly.graph_objects as go
 
 write_dir = Path("data/eval_pipeline")
-def create_model_csvs():
+
+
+def create_model_csvs(models: list[str]):
 
     data_path = Path("data/statements_filtered.csv")
 
@@ -20,7 +23,7 @@ def create_model_csvs():
     data = load_data(data_path, "classification_acc")
 
     device = "cpu"
-    model_names = ["text-ada-001", "text-babbage-001", "text-curie-001", "text-davinci-001"]
+    model_names = models
     for model_name in tqdm(model_names):
         run_model(model_name, data, write_dir, device, 100, "classification_acc")
 
@@ -36,6 +39,7 @@ def create_model_csvs():
         )
     labelled_path = Path(write_dir, "data.jsonl")
     labelled_df.to_json(labelled_path, orient="records", lines=True)
+
 
 def extract_classification_result(path: Path) -> list[bool]:
     """
@@ -59,38 +63,75 @@ def calculate_accuracy(data: list[tuple[str, list[bool]]]) -> pd.DataFrame:
     return pd.DataFrame(accuracies)
 
 
-def plot_accuracy_chart(accuracy_df: pd.DataFrame):
-    fig = px.line(accuracy_df, x='model', y='accuracy', title='Model Accuracy', markers=True)
+def plot_accuracy_chart(vanilla_df: pd.DataFrame, feedme_df: pd.DataFrame):
+    fig = go.Figure()
+
+    # Add red line for the first dataframe
+    fig.add_trace(
+        go.Scatter(
+            x=vanilla_df["model"],
+            y=vanilla_df["accuracy"],
+            mode="lines+markers",
+            line=dict(color="blue"),
+            name="Vanilla",
+        )
+    )
+    # Add second line for the second dataframe
+    fig.add_trace(
+        go.Scatter(
+            x=feedme_df["model"],
+            y=feedme_df["accuracy"],
+            mode="lines+markers",
+            line=dict(color="red"),
+            name="FeedMe",
+        )
+    )
 
     # Add baseline
     fig.add_shape(
-        type='line',
+        type="line",
         x0=0,
-        x1=len(accuracy_df) - 1,
+        x1=len(vanilla_df) - 1,
         y0=0.5,
         y1=0.5,
-        yref='y',
-        xref='x',
-        line=dict(color='black', dash='dot')
+        yref="y",
+        xref="x",
+        line=dict(color="black", dash="dot"),
     )
     # make the y axis start at 0 and end at 1
     fig.update_yaxes(range=[0, 1])
     return fig
 
 
+vanilla_models = ["ada", "babbage", "curie", "davinci"]
+feedme_models = [
+    "text-ada-001",
+    "text-babbage-001",
+    "text-curie-001",
+    "text-davinci-001",
+]
+
+
 def plot_eval_loss():
-    extracted_results = []
-    for model_name in ["text-ada-001", "text-babbage-001", "text-curie-001", "text-davinci-001"]:
+    vanilla_results = []
+    for model_name in vanilla_models:
         path = Path(write_dir, model_name + ".csv")
         results = extract_classification_result(path)
-        extracted_results.append((model_name, results))
-    acc_df = calculate_accuracy(extracted_results)
-    plot = plot_accuracy_chart(acc_df)
+        vanilla_results.append((model_name, results))
+    vanilla_df = calculate_accuracy(vanilla_results)
+    feedme_results = []
+    for model_name in feedme_models:
+        path = Path(write_dir, model_name + ".csv")
+        results = extract_classification_result(path)
+        # take the truncate name e.g. text-ada-001 -> ada
+        truncated_model_name = model_name.split("-")[1]
+        feedme_results.append((truncated_model_name, results))
+    feedme_df = calculate_accuracy(feedme_results)
+    plot = plot_accuracy_chart(vanilla_df, feedme_df)
     # save to png
     plot.write_image("data/eval_pipeline/eval_pipeline_accuracy.png")
 
 
-
 if __name__ == "__main__":
-    # create_model_csvs()
+    # create_model_csvs(models=feedme_models + vanilla_models)
     plot_eval_loss()
